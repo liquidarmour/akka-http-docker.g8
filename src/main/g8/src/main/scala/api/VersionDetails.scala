8323499b02ee7156f3d4cacd7c616c6bfd86722a
James Williams <james@liquid-armour.co.uk>
2017-06-11 19:40:41 +0100

    Adding classes for base server and version information.

---
+++ src/main/g8/src/main/scala/api/VersionDetails.scala
@@ -0,0 +1,31 @@
+package abct.corporation.tax.api
+
+import java.util.jar.Attributes
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json._
+
+case class VersionDetails(
+  applicationName: String,
+  applicationOwner: String,
+  applicationVersion: String,
+  gitVersion: String,
+  releasedVersion: Boolean
+)
+
+object VersionDetails extends SprayJsonSupport with DefaultJsonProtocol {
+
+  implicit val VersionDetailsFormat = jsonFormat5(VersionDetails.apply)
+
+  def fromAttributes(attirbutes: Attributes): VersionDetails = {
+    val version = attirbutes.getValue("Implementation-Version")
+    val gitVersion = attirbutes.getValue("Git-Version")
+    VersionDetails(
+      applicationName = attirbutes.getValue("Implementation-Title"),
+      applicationOwner = attirbutes.getValue("Specification-Vendor"),
+      applicationVersion = version,
+      gitVersion = gitVersion,
+      releasedVersion = gitVersion == version
+    )
+  }
+}
