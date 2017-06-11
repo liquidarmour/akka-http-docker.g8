f581aeb82f35002a13f11577123c22eb7ae9d613
James Williams <james@liquid-armour.co.uk>
2017-06-11 19:43:33 +0100

    Adding classes for base server and version information.

---
+++ src/main/g8/src/main/scala/routes/VersionRoutes.scala
@@ -0,0 +1,42 @@
+package abct.corporation.tax.routes
+
+import java.io.IOException
+import java.net.{ URL, URLClassLoader }
+import java.util.jar.{ Attributes, Manifest }
+
+import abct.corporation.tax.api.VersionDetails
+import akka.http.scaladsl.server.directives.MethodDirectives.get
+import akka.http.scaladsl.server.directives.PathDirectives.path
+import akka.http.scaladsl.server.directives.RouteDirectives.complete
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json._
+
+trait VersionRoutes {
+
+  lazy val versionRoutes =
+    path("version") {
+      get {
+
+        val versionInformation: Option[VersionDetails] = readManifest.map { manifest =>
+          val mainAttributes: Attributes = manifest.getMainAttributes
+          VersionDetails.fromAttributes(mainAttributes)
+        }
+
+        import VersionDetails._
+        complete(versionInformation.toJson)
+      }
+    }
+
+  lazy val readManifest: Option[Manifest] = {
+    val clazz = getClass
+    val className = clazz.getSimpleName + ".class"
+    val classPath = clazz.getResource(className).toString
+    if (!classPath.startsWith("jar")) {
+      None
+    } else Some {
+      val manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) + "/META-INF/MANIFEST.MF"
+      new Manifest(new URL(manifestPath).openStream())
+    }
+  }
+
+}
